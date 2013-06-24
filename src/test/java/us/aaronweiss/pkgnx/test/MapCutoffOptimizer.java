a2e22df8e0d3ea0abd7da9f298796ead2427b5c1
Aaron Weiss <aaronweiss74@gmail.com>
2013-06-24 02:57:02 -0400

    Added an optimizer to determine hashmap cutoff.

--- src/test/java/us/aaronweiss/pkgnx/test/MapCutoffOptimizer.java
+++ src/test/java/us/aaronweiss/pkgnx/test/MapCutoffOptimizer.java
@@ -1,9 +1,119 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (C) 2013 Aaron Weiss
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package us.aaronweiss.pkgnx.test;
+
+import com.google.common.base.Stopwatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import us.aaronweiss.pkgnx.NXFile;
+import us.aaronweiss.pkgnx.format.NXNode;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Aaron
  * @version 1.0
- * @since 6/24/13
+ * @since 6/23/13
  */
 public class MapCutoffOptimizer {
+	public static final Logger logger = LoggerFactory.getLogger(SmartRecurseAndSearch.class);
+	public static final String FILE_PATH = "src/test/resources/Data-do.nx";
+	public static final Stopwatch timer = new Stopwatch();
+	public static final int TRIALS = 0x20;
+
+	/**
+	 * Runs the advanced Map Cutoff Optimizer.
+	 *
+	 * @param args ignored
+	 */
+	public static void main(String[] args) throws IOException {
+		long lowest = Long.MAX_VALUE;
+		int lowestId = 12;
+		for (int k = 12; k <= 100; k++) {
+			// n.b. this is an optimization test, you must make MIN_COUNT_FOR_MAPS non-final and public first
+			// NXNode.MIN_COUNT_FOR_MAPS = k;
+			ResultSet rs1 = new ResultSet(TRIALS);
+			NXFile file = null;
+			for (int i = 0; i < TRIALS; i++) {
+				timer.start();
+				file = new NXFile(FILE_PATH, NXFile.LibraryMode.MAPPED_AND_PARSED);
+				timer.stop();
+				long time = timer.elapsed(TimeUnit.MICROSECONDS);
+				rs1.add(time);
+				timer.reset();
+			}
+			ResultSet rs2 = new ResultSet(TRIALS);
+			for (int i = 0; i < TRIALS; i++) {
+				timer.start();
+				recurse(file.getRoot());
+				timer.stop();
+				long time = timer.elapsed(TimeUnit.MICROSECONDS);
+				rs2.add(time);
+				timer.reset();
+			}
+			long product = rs1.getAverage() * rs2.getAverage();
+			if (product < lowest) {
+				lowest = product;
+				lowestId = k;
+			}
+			logger.info("[RS] " + k + " " + product);
+		}
+		logger.info("[RS] BEST: " + lowestId + " " + lowest);
+	}
+
+	/**
+	 * Recurses through all the nodes and records information about the SS benchmark on them.
+	 *
+	 * @param node the node to recurse on
+	 */
+	private static void recurse(NXNode node) {
+		SS(node);
+		for (NXNode child : node) {
+			recurse(child);
+		}
+	}
+
+	/**
+	 * Runs retep998's SS benchmark on a specific node.
+	 * <p/>
+	 * SS: String search; time taken to iterate through the  children of a {@code trialNode},
+	 * access each child by name, and compare the indexed child to the iterated child.
+	 *
+	 * @param trialNode the node to perform the SS trial on.
+	 * @return time for the trial
+	 */
+	public static void SS(NXNode trialNode) {
+		try {
+			for (NXNode child : trialNode) {
+				if (!child.equals(trialNode.getChild(child.getName())))
+					throw new RuntimeException("pkgnx is failing to work completely.");
+			}
+		} catch (Exception e) {
+			logger.error("[SS] trial failed with an exception.", e);
+		}
+		return;
+	}
 }
