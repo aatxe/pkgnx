fde1ebe4a5c276b6a049730c8830a0a5e8debf11
Aaron Weiss <aaronweiss74@gmail.com>
2014-01-24 15:14:21 -0500

    Updated copyright years across the library.

--- src/main/java/us/aaronweiss/pkgnx/util/NodeParser.java
+++ src/main/java/us/aaronweiss/pkgnx/util/NodeParser.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (C) 2013 Aaron Weiss
+ * Copyright (C) 2014 Aaron Weiss
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
